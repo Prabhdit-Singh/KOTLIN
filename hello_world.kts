2fcb429771b01b231f4ddb3cbff58816c62f3a50
Prabhdit-Singh <54041431+Prabhdit-Singh@users.noreply.github.com>
2021-05-23 15:21:44 +0530

    Hello World program added

--- hello_world.kts
+++ hello_world.kts
@@ -1,1 +1,8 @@
-println("Hello World! We are going to learn KOTLIN...")
+fun main() {
+    var str: String = "Hello World! This is our first ever KOTLIN program"
+    var name: String = "Prabhdit Singh"
+    val age: Int = 23
+    println(str)
+    println("My name is "+ name+ " and I am "+ age+ " years old.")
+}
+main()
